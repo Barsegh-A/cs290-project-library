4781812ab1b07530bec0aa16bcb7e425457609b6
barseghatanyan <barsegh.atanyan@tumo.org>
2022-05-14 18:55:32 +0400

    move mongo db connection url to .env

--- server/app.js
+++ server/app.js
@@ -1,14 +1,19 @@
 const errorHandler = require('./common/middlewares/error-handler.middleware');
 const express = require('express');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
 
-const dbURI = "mongodb+srv://admin:library-cs290@cs290-project.4wmcs.mongodb.net/library?retryWrites=true&w=majority";
-
 async function bootstrap() {
-    await mongoose.connect(dbURI);
+    console.log(process.env.MONGODB_URL);
+
+    await mongoose.connect(process.env.MONGODB_URL);
+
     console.log('DB connected successfully.');
+
     app.listen(3000, () => {
         console.log('Server is running! 🚀');
     })
